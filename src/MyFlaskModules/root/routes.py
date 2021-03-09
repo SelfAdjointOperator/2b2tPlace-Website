631d3ac0051f76ec16a94d793ec3a26d9ae5b20e
Bateman <66702639+SelfAdjointOperator@users.noreply.github.com>
2021-03-09 01:51:07 +0000

    move form from root to api, used for validation only, remove jquery and unused files

--- src/MyFlaskModules/root/routes.py
+++ src/MyFlaskModules/root/routes.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, render_template, url_for, request
 
 from . import MODULE_CONFIG
-from .forms import Form_SubmitPixel
 
 bp = Blueprint("root",
     __name__,
@@ -11,12 +10,6 @@ bp = Blueprint("root",
     static_url_path = "root/static"
 )
 
-@bp.route("/") # methods = ["GET", "POST"]
+@bp.route("/")
 def index():
-    form = Form_SubmitPixel(request.form)
-    # if request.method == "POST":
-    #     handle_Form_SubmitPixel(form)
-    #     return redirect(url_for(".index"))
-    # elif request.method == "GET": # TODO remove all this and use JS to query API
-    #, form = form
     return render_template("root/index.html")
--- src/MyFlaskModules/root/static/js/rebane2001/NB.md
+++
@@ -1,3 +0,0 @@
-Work taken from and given to [Rebane](https://github.com/rebane2001/mapartcraft)
-
-All kept open in accordance with GPL v3.0
